1f441e64d91bed5c9cd91c3ead924ae4fc3d03f6
Gopika Krishnan <99195419+G93pika@users.noreply.github.com>
2022-02-22 21:47:48 +0100

    Add files via upload

---
+++ Continued inspecting HTML.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+driver = webdriver.Chrome(service=Service("/Library/Frameworks/Python.framework/Versions/3.10/chromedriver"))
+driver.get('https://rahulshettyacademy.com/angularpractice/')
+#driver.find_element(By. NAME,"name").send_keys("Gopika")
+driver.find_element(By.CSS_SELECTOR,"input[name='name']").send_keys("Gopika")
+driver.find_element(By.NAME,"email").send_keys("gopika.k.1993@gmail.com")
+driver.find_element(By.CLASS_NAME,"form-check-label").click()
+driver.find_element(By.ID,"exampleInputPassword1").send_keys("Password")
+dropdown = Select(driver.find_element(By.ID, "exampleFormControlSelect1"))
+dropdown.select_by_visible_text("Female")
+driver.find_element(By.XPATH,"//input[@type='submit']").click()
+print(driver.find_element(By.CLASS_NAME,"alert-success").text)
+driver.quit()
